45dc0f6b6132c7641eca23c37557b27e168aae78
Matt <matt.corsaro1@gmail.com>
2018-05-09 23:47:35 -0400

    Plot averages from TensorBoard

--- plots_from_boards.py
+++ plots_from_boards.py
@@ -16,46 +16,49 @@ def main():
     parser = argparse.ArgumentParser(description = "Generate plots from TensorBoard log files")
     # If true, generates plots for 
     parser.add_argument("--logdir", type=str, default="/home/matt/boards_ll")
-    parser.add_argument('--skillchain', dest='skillchain', action='store_true')
-    parser.add_argument('--no-skillchain', dest='skillchain', action='store_false')
-    parser.set_defaults(skillchain=True)
     args = parser.parse_args()
 
-    path = args.logdir + '/' + ("sc" if args.skillchain else "dqn") + '/'
-    print "Reading TensorBoard files from", path
-    all_rewards = []
-    for subdir, dirs, files in os.walk(path):
-        for file in files:
-            if file[:10] == "events.out" and (subdir[-9:] == "GlobalMDP" or not args.skillchain):
-                log_file = subdir + '/' + file
-                print log_file
-                ea = event_accumulator.EventAccumulator(log_file)
-                ea.Reload()
-                for tag in ea.Tags()['scalars']:
-                    if tag == "Episode_Reward":
-                        all_rewards.append(np.array([thing.value for thing in ea.Scalars(tag)]))
-                    elif tag == "Epsilon":
-                        #TODO
-                        pass
-    np_all_rewards = np.array(all_rewards)
+    sc_path = args.logdir + '/' + "sc" + '/'
+    dqn_path = args.logdir + '/' + "dqn" + '/'
+    rewards = [[], []]
+    for i, t in enumerate([sc_path, dqn_path]):
+        for subdir, dirs, files in os.walk(t):
+            for file in files:
+                if file[:10] == "events.out" and (subdir[-9:] == "GlobalMDP" or i):
+                    log_file = subdir + '/' + file
+                    print "Reading", log_file
+                    ea = event_accumulator.EventAccumulator(log_file)
+                    ea.Reload()
+                    for tag in ea.Tags()['scalars']:
+                        if tag == "Episode_Reward":
+                            rewards[i].append(np.array([run.value for run in ea.Scalars(tag)]))
+                        elif tag == "Epsilon":
+                            #TODO
+                            pass
+    sc_rewards = np.array(rewards[0])
+    dqn_rewards = np.array(rewards[1])
 
-    '''
-    ea = event_accumulator.EventAccumulator("/home/matt/boards_ll/dqn/board_2018_05_09_08_25_19/events.out.tfevents.1525868720.gpu1703")
-    ea.Reload()
-    for tag in ea.Tags()['scalars']:
-        # unicode
-        print str(tag)
-    """
-    for scalar in ea.Scalars('Episode_Reward'):
-        print scalar.step, scalar.value
-    """
-    '''
+    # uncorrected sample sd, population vs sample variance
+    sc_avg = np.mean(sc_rewards, axis=0)
+    sc_std = np.std(sc_rewards, axis=0)
+    dqn_avg = np.mean(dqn_rewards, axis=0)
+    dqn_std = np.std(dqn_rewards, axis=0)
 
-    '''
-    pl.plot(x, y, 'k-')
-    pl.fill_between(x, y-error, y+error)
-    pl.show()
-    '''
+    print sc_avg[-1], dqn_avg[-1]
+
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d_%H_%M_%S')
+    
+    plt.xlim([0, 1000])
+    plt.ylim([-400, 400])
+    plt.plot(range(dqn_avg.shape[0]), dqn_avg, 'b')
+    plt.fill_between(range(dqn_avg.shape[0]), dqn_avg-dqn_std, dqn_avg+dqn_std)
+    plt.savefig(args.logdir + '/' + timestamp + '_dqn.png')
+    plt.close()
+    plt.xlim([0, 1000])
+    plt.ylim([-400, 400])
+    plt.plot(range(sc_avg.shape[0]), sc_avg, 'g')
+    plt.fill_between(range(sc_avg.shape[0]), sc_avg-sc_std, sc_avg+sc_std)
+    plt.savefig(args.logdir + '/' + timestamp + '_sc.png')
 
 
 if __name__ == '__main__':
